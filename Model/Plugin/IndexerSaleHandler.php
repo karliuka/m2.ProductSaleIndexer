88699266a701ec179412769c32d19ee9ab484830
karliuka <wit.kharkov@gmail.com>
2017-05-06 18:31:14 +0300

    plugin

---
+++ Model/Plugin/IndexerSaleHandler.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Faonni
+ *  
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade module to newer
+ * versions in the future.
+ * 
+ * @package     Faonni_ProductSale
+ * @copyright   Copyright (c) 2017 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Faonni\ProductSale\Model\Plugin;
+
+use Faonni\ProductSale\Model\Indexer\Product\SaleIndexer;
+
+/**
+ * ProductSale plugin
+ */
+class IndexerSaleHandler
+{
+    /**
+     * Sale Indexer instance
+     *
+     * @var \Faonni\ProductSale\Model\Indexer\Product\SaleIndexer
+     */
+    protected $_indexer;         
+    
+    /**
+	 * Initialize plugin
+	 *	
+     * @param SaleIndexer $indexer
+     */
+    public function __construct(
+        SaleIndexer $indexer
+    ) {
+        $this->_indexer = $indexer;
+    }
+
+    /**
+     * Execute partial indexation by ID list
+     *
+     * @param \Magento\Catalog\Model\Indexer\Product\Price $rule
+     * @param int[] $ids
+     * @return int[] $ids
+     */     
+    public function beforeExecuteList($subject, array $ids)
+    {
+
+        return $ids;
+    }
+}  
